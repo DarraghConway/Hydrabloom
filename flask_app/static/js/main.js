f9954597862d0b40265e07a7e9fe41f4e295eb22
Darragh24 <conwaydarragh03@gmail.com>
2024-12-05 14:54:40 +0000

    🔨 modified diplay_data so it now also shows lux metric , updated the main.js so it reads the lux metric from Pubnub also

--- flask_app/static/js/main.js
+++ flask_app/static/js/main.js
@@ -3,9 +3,9 @@ let appChannel = "dht22-pi-channel";
 
 const setupPubNub = () => {
   pubnub = new PubNub({
-    publishKey: "pub-c-5e5be15e-f582-40f2-8c1a-ca9360199f9d",  // Replace with your actual publish key
-    subscribeKey: "sub-c-a2447194-c69b-4dbe-a5e8-a4ed24799246",  // Replace with your actual subscribe key
-    userId: "Jack-device",  // Replace with your actual user ID
+    publishKey: "pub-c-5e5be15e-f582-40f2-8c1a-ca9360199f9d", // Replace with your actual publish key
+    subscribeKey: "sub-c-a2447194-c69b-4dbe-a5e8-a4ed24799246", // Replace with your actual subscribe key
+    userId: "Jack-device", // Replace with your actual user ID
   });
 
   // Subscribe to the channel
@@ -19,15 +19,26 @@ const setupPubNub = () => {
       console.log("Status:", s.category);
     },
     message: (messageEvent) => {
-      handleMessage(messageEvent.message);  // Handle the incoming message
+      handleMessage(messageEvent.message); // Handle the incoming message
     },
   });
 };
 
 function handleMessage(message) {
-  // Check if both temperature and humidity keys exist in the message
+  // Check if temperature and humidity keys exist in the message
   if (message.temperature && message.humidity) {
-    document.getElementById("temperature_id").innerHTML = `${message.temperature.toFixed(1)}°C`;
-    document.getElementById("humidity_id").innerHTML = `${message.humidity.toFixed(1)}%`;
+    document.getElementById(
+      "temperature_id"
+    ).innerHTML = `${message.temperature.toFixed(1)}°C`;
+    document.getElementById(
+      "humidity_id"
+    ).innerHTML = `${message.humidity.toFixed(1)}%`;
+  }
+
+  // Check if lux value is available in the message
+  if (message.lux !== undefined) {
+    document.getElementById("lux_id").innerHTML = `${message.lux.toFixed(
+      1
+    )} lux`;
   }
 }
