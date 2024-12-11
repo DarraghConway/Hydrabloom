0c90639a9855b9fc968d7cb59e52ab331d24ee86
ehima <ehimareataga063@gmail.com>
2024-12-11 11:28:40 +0000

    Initial test commits

--- flask_app/db.py
+++ flask_app/db.py
@@ -2,17 +2,17 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# Initialize the SQLAlchemy object
 db = SQLAlchemy()
 migrate = Migrate()
 
-# Models
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    plants = db.relationship('Plant', backref='user', lazy=True)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)  
+    plants = db.relationship('Plant', backref='user', lazy=True, cascade='all, delete-orphan') 
+
 
 class PlantType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,9 +27,13 @@ class Plant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     plant_type_id = db.Column(db.Integer, db.ForeignKey('plant_type.id'), nullable=False)
-    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)  
     date_planted = db.Column(db.Date, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    location = db.relationship('Location', backref='plants', lazy=True)
+    plant_type = db.relationship('PlantType', backref='plants', lazy=True)
+
 
 
 class TempAndHumidityData(db.Model):
@@ -38,8 +42,6 @@ class TempAndHumidityData(db.Model):
     temperature = db.Column(db.Float, nullable=False)
     humidity = db.Column(db.Float, nullable=False)
 
-
-# A function to initialize the database and migrations
 def init_db(app):
     db.init_app(app)
     migrate.init_app(app, db)
